9e9b8f8c6b71b907ae68d261e0e63698eb74e300
loet3720 <tyler.loewen@uleth.ca>
2018-03-27 13:12:59 -0600

    Update racket source to remove top level lambda

--- uleth.rkt
+++ uleth.rkt
@@ -1,14 +1,12 @@
 #lang racket
 
 (define uofl
-  (lambda (balance)
     (display "UofL>")
     (set! balance (list (read-line)))
     (display balance)
     (lambda (n)
       (display "this is lambda2")
       (set! balance (+ balance (n)))
-      balance)))
-
-
-(uofl 7)
+      balance
+    )
+)
