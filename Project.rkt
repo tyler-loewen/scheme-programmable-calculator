c10e5f7adad535dac52a9ee4a36473010959fcb5
loet3720 <tyler.loewen@uleth.ca>
2018-04-10 17:27:50 -0600

    Add control statement evaluation support

--- Project.rkt
+++ Project.rkt
@@ -5,8 +5,9 @@
          (prefix-in : parser-tools/lex-sre))
 
 (define-tokens value-tokens (CONST-INT CONST-FLOAT CONST-BOOL NAME SYS-CALL))
+(define-empty-tokens control-tokens (IF THEN ELSE ELSEIF ENDIF))
 (define-empty-tokens op-tokens (+ - / * ^ NEG = == >= <= <> > <))
-(define-empty-tokens delim-tokens (NEWLINE EOF HASH OP CP WHITESPACE COMMENT))
+(define-empty-tokens delim-tokens (EOF HASH OP CP WHITESPACE COMMENT))
 
 (define vars (make-hash))
 (define funcs (make-hash))
@@ -26,15 +27,19 @@
    ((:or "+" "-" "/" "*" "^" "=" ">" "<") (string->symbol lexeme))
    (#\( 'OP)
    (#\) 'CP)
-   (#\newline 'NEWLINE)
-   ((:or #\tab #\space) 'WHITESPACE)
-   (line-comment 'COMMENT)
-   ("true" (token-CONST-BOOL (#t)))
-   ("false" (token-CONST-BOOL (#f)))
-   ((:: #\# (:+ letter)) (token-SYS-CALL (list->string lexeme)))
-   ((:+ letter) (token-NAME (string->symbol lexeme)))
-   ((:: (:+ digit) #\. (:* digit)) (token-CONST-FLOAT (string->number lexeme)))
-   ((:+ digit) (token-CONST-INT (string->number lexeme)))
+   ((:or #\tab #\space #\newline) (uofl-lexer input-port)) ;; ignore whitespace
+   (line-comment 'COMMENT) ;; comment
+   ("true" (token-CONST-BOOL (#t))) ;; true
+   ("false" (token-CONST-BOOL (#f))) ;; false
+   ("if" 'IF)
+   ("then" 'THEN)
+   ("elseif" 'ELSEIF)
+   ("else" 'ELSE)
+   ("endif" 'ENDIF)
+   ((:: #\# (:+ letter)) (token-SYS-CALL (list->string lexeme))) ;; system call
+   ((:+ letter) (token-NAME (string->symbol lexeme))) ;; variable/function
+   ((:: (:+ digit) #\. (:* digit)) (token-CONST-FLOAT (string->number lexeme))) ;; float
+   ((:+ digit) (token-CONST-INT (string->number lexeme))) ;; integer
    ((eof) 'EOF)
   )
 )
@@ -60,49 +65,85 @@
     (left NEG)
     (right ^)
    )
-   (tokens value-tokens op-tokens delim-tokens)
+   (tokens value-tokens op-tokens delim-tokens control-tokens)
    (grammar
     (s (() #f)
        ((exp) $1))
     (exp
-     ((CONST-INT) $1)
-     ((CONST-FLOAT) $1)
-     ((CONST-BOOL) $1)
-     ((NAME) (hash-ref vars $1 0))
-     ((NAME = exp) (begin
+     ((CONST-INT) (lambda () $1))
+     ((CONST-FLOAT) (lambda () $1))
+     ((CONST-BOOL) (lambda () $1))
+     ((NAME) (lambda () (hash-ref vars $1 0)))
+     ((NAME = exp) (lambda ()
                      (hash-set! vars $1 $3)
                      $3
                    ))
-     ((exp + exp) (+ $1 $3))
-     ((exp - exp) (- $1 $3))
-     ((exp * exp) (* $1 $3))
-     ((exp / exp) (/ $1 $3))
-     ((- exp) (prec NEG) (- $2))
-     ((exp ^ exp) (expt $1 $3))
-     ((exp == exp) (= $1 $3))
-     ((exp <> exp) (not(eq? $1 $3)))
-     ((exp >= exp) (>= $1 $3))
-     ((exp <= exp) (<= $1 $3))
-     ((exp > exp) (> $1 $3))
-     ((exp < exp) (< $1 $3))
-     ((OP exp CP) $2)
+     ((exp + exp) (lambda () (+ $1 $3)))
+     ((exp - exp) (lambda () (- $1 $3)))
+     ((exp * exp) (lambda () (* $1 $3)))
+     ((exp / exp) (lambda () (/ $1 $3)))
+     ((- exp) (prec NEG) (lambda () (- $2)))
+     ((exp ^ exp) (lambda () (expt $1 $3)))
+     ((exp == exp) (lambda () (eq? $1 $3)))
+     ((exp <> exp) (lambda () (not(eq? $1 $3))))
+     ((exp >= exp) (lambda () (>= $1 $3)))
+     ((exp <= exp) (lambda () (<= $1 $3)))
+     ((exp > exp) (lambda () (> $1 $3)))
+     ((exp < exp) (lambda () (< $1 $3)))
+     ((OP exp CP) (lambda () $2))
+     ((IF exp THEN exp ENDIF) (lambda()
+                                (if (execute $2)
+                                    (lambda () $4)
+                                    (lambda () 0)
+                                )))
      ))))
-           
-;; run the calculator on the given input-port       
-(define (calc ip)
+
+(define (lex-all-recursive ip token-list)
+  (let ((tok (uofl-lexer ip)))
+    (set! token-list (append token-list (list tok)))
+    (if (not (eq? tok 'EOF))
+        (lex-all-recursive ip token-list)
+        (lambda ()
+          token-list
+        )
+    )
+  )
+)
+
+(define (parse-list token-list)
+  (printf "Tokenized: ~a\n" token-list)
+  (let ((parse-object (uofl-parser
+                       (lambda ()
+                         (let ((tok (car token-list)))
+                           (set! token-list (cdr token-list))
+                           tok
+                           )
+                         )
+                       )))
+    parse-object
+  )
+)
+
+(define (execute parse-object)
+  (if (procedure? parse-object)
+      (execute (parse-object))
+      parse-object
+  )
+)
+
+(define (lex-parse-all ip)
+  (let* ((token-list '())
+        (result (lex-all-recursive ip token-list)))
+    (execute (parse-list (result)))
+  )
+)
+
+(define (interpret ip)
   (port-count-lines! ip)
-  (letrec ((one-line
-	    (lambda ()
-	      (let ((result (uofl-parser (lambda () (uofl-lexer ip)))))
-                (cond (result) (printf "~a\n" result)(one-line)
-                      (else (printf "`a\n" result) (one-line)))
-                ))))
-    (one-line)))
+  (let ((temp (lex-parse-all ip)))
+    (printf "Result: ~a\n" temp)
+  )
+)
 
-(define (uofl)
-  (display "UofL>")
-  (calc (open-input-string (read-line))))
-;;(calc (open-input-string "x=1+2-5"))
-;;(calc (open-input-string "x=7*2.0/6.0"))
-
-(uofl)
+(interpret (open-input-string "if (x == 0) then x=1 endif"))
+;;(interpret (open-input-string "if (x == 0) then\nx = 2\nendif"))
